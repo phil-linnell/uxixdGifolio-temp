6bdbe0b051dbc6723a3b6d552917d31d7b22ab3d
Phil Linnell <phil@ustwo.com>
2023-07-28 09:54:18 +0100

    Fix

--- content/gifs.js
+++ content/gifs.js
@@ -1,95 +1,96 @@
 const gifs = [
- {
+  {
     day: "018",
     content: "Move map under pin, rather than move pin. A green closing apeture reaffrims focus on selection.",
     client: "Ford / ustwo",
     gif: "fordgoparkzone.mp4"
   }, 
- {
+  {
     day: "017",
     content: "Special screens use sound waves so blind people can feel the buttons on a flat touchscreen.",
     client: "Volkswagen / ustwo",
     gif: "vwblindscreen.mp4"
   }, 
- {
+  {
     day: "016",
     content: "An AR car configurator with life-size Land Rover so you can picture it in your driveway.",
     client: "JLR / ustwo",
     gif: "jlrar.mp4"
   }, 
- {
+  {
     day: "015",
     content: "An unboxing narrative that releases components in order of installation steps.",
     client: "PlayStation / GR/DD",
     gif: "playtvunbox.mp4"
   }, 
- {
+  {
     day: "014",
     content: "Using facial recognition to detect when the HMI needs to display for the driver.",
     client: "Toyota / ustwo",
     gif: "toyotafocus.mp4"
   },  
-{
+ {
     day: "013",
-    content: "Gestures in the car can be problematic. We explored a "gesture zone" to control the interaction method.",
+    content: "Gestures in the car can be problematic. We explored a 'gesture zone' to control the interaction method.",
     client: "Toyota / ustwo",
     gif: "toyotavolume.mp4"
   },  
- {
+  {
     day: "012",
     content: "Motion sickness happens when our balance and vision don't match. This game of Alexa I Spy solves that using real word items from around the car.",
     client: "Ford / HMSD",
     gif: "fordechoauto.mp4"
   },  
- {
+  {
     day: "011",
     content: "Connected homes with contextual presets. Ie. Away Mode, Cosy Mode, Part Mode etc.",
     client: "Garmin",
     gif: "garminhome.mp4"
   },  
- {
+  {
     day: "010",
     content: "Contextual surface displays for pairing and authorisation feedback.",
     client: "Garmin",
     gif: "garmincar.mp4"
   },  
- {
+  {
     day: "009",
     content: "Contextual actions. Directions back to your car, with beep and flash offered upon approach.",
     client: "JLR / ustwo",
     gif: "jlr.mp4"
   },  
-   {
+  {
     day: "008",
     content: "E-ink tech means utilisation of a single autonomous vehicle can shift to the cities demands throughout the day.",
     client: "JLR / ustwo",
     gif: "roo.mp4"
   },  
-   {
+   
+  {
     day: "007",
     content: "Start your car, from your watch - like James Bond.",
     client: "JLR / ustwo",
     gif: "jlrremote.mp4"
   },  
-   {
+  {
     day: "006",
     content: "Bluetooth scent emmiter protoype - adding smell ot VR experiences.",
     client: "NewTerritory",
     gif: "spray.mp4"
   },  
-   {
+  {
     day: "005",
     content: "Is a needle pointing at a number the best way to show speed?",
     client: "ustwo",
     gif: "ferrari.mp4"
   },
-   {
+  {
     day: "004",
     content: "Context is key(board). An on-screen keyboard with the space bar centred can save input time.",
     client: "Samsung / AllofUs",
     gif: "keyboard.mp4"
   },
-   {
+  {
     day: "003",
     content: "Decision fatigue? Select an option at random with a 'Surprise me' option.",
     client: "Micro but Many",
