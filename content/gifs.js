b87cb2e46922cfa662537837b1b787e82990a2ee
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-07-31 01:58:03 +0100

    Update gifs.js

--- content/gifs.js
+++ content/gifs.js
@@ -6,11 +6,11 @@ const gifs = [
     gif: "jlrarhmi.mp4"
   },  
   {
-    day: "030",
-    content: "Real sand is driven into reclaimed tyres for this off-road racing game.",
-    client: "PlayStation",
-    gif: "sandkit.mp4"
-  }, 
+     day: "030",
+     content: "IXD can bring brand delight to packaging - or a raspberry from a virtual pet.",
+     client: "PlayStation",
+     gif: "eyepet.mp4"
+  },  
   {
     day: "029",
     content: "Gestures in the car can be problematic. We explored a 'gesture zone' to rein in the accuracy.",
@@ -24,11 +24,11 @@ const gifs = [
      gif: "greenhouse.mp4"
   }, 
     {
-     day: "027",
-     content: "IXD can bring brand delight to packaging - or a raspberry from a virtual pet.",
-     client: "PlayStation",
-     gif: "eyepet.mp4"
-  },  
+    day: "027",
+    content: "Real sand is driven into reclaimed tyres for this off-road racing game.",
+    client: "PlayStation",
+    gif: "sandkit.mp4"
+  }, 
   {
     day: "026",
     content: "Is a needle pointing at a number the best way to show speed? We explore alternatives.",
