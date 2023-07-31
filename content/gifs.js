a33bd3b6e2f20945a5eae23ed1a68b38ef3181be
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-07-31 08:08:37 +0100

    Update gifs.js

--- content/gifs.js
+++ content/gifs.js
@@ -1,7 +1,7 @@
 const gifs = [
    {
     day: "036",
-    content: "Reductionist approach for glanceable turn-by-turn motorcycle navigation.",
+    content: "Reductionist approach to glanceable turn-by-turn motorcycle navigation.",
     client: "Ducati",
     gif: "ducaticluster.mp4"
   },  
