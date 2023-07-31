39829c580c8e63ba130b50a1c7410ed966564a85
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-07-31 01:05:04 +0100

    Update gifs.js

--- content/gifs.js
+++ content/gifs.js
@@ -8,7 +8,7 @@ const gifs = [
   {
     day: "029",
     content: "Gestures in the car can be problematic. We explored a 'gesture zone' to rein in the accuracy.",
-    client: "Toyota / ustwo",
+    client: "Toyota | ustwo",
     gif: "toyotavolume.mp4"
   },  
     {
@@ -32,7 +32,7 @@ const gifs = [
   {
      day: "025",
      content: "Using VR to safely test our electric vehicle sound designs with doppler effect.",
-     client: "Nissan / ustwo",
+     client: "Nissan | ustwo",
      gif: "evsoundtest.mp4"
   }, 
   {
@@ -44,7 +44,7 @@ const gifs = [
   {
      day: "023",
      content: "Move map under pin, rather than move pin. A green aperture reaffrims focus on selection.",
-     client: "Ford / ustwo",
+     client: "Ford | ustwo",
      gif: "fordgoparkzone.mp4"
   }, 
   {
@@ -56,7 +56,7 @@ const gifs = [
   {
      day: "021",
      content: "A metal cube levitates with electro-magnets + precision weight distribution.",
-     client: "Huawei / AllofUs",
+     client: "Huawei | AllofUs",
      gif: "huaweicube.mp4"
   }, 
   {
@@ -68,7 +68,7 @@ const gifs = [
     {
     day: "019",
     content: "Contextual actions. Directions back to your car, with beep and flash offered upon approach.",
-    client: "JLR / ustwo",
+    client: "JLR | ustwo",
     gif: "jlr.mp4"
   }, 
   {
@@ -80,7 +80,7 @@ const gifs = [
   {
      day: "017",
      content: "Architect your own floorplan as you explore the architect's digital showroom.",
-     client: "CenterPlan / Unit9",
+     client: "CenterPlan | Unit9",
      gif: "centerplanbuild.mp4"
   }, 
   {
@@ -92,7 +92,7 @@ const gifs = [
   {
     day: "015",
     content: "An AR car configurator with life-size Land Rover so you can picture yours in your driveway.",
-    client: "JLR / ustwo",
+    client: "JLR | ustwo",
     gif: "jlrar.mp4"
   }, 
   {
@@ -104,7 +104,7 @@ const gifs = [
   {
      day: "013",
      content: "Concept for an interactive touch sphere using interior projection and touch-sensitive mesh.",
-     client: "NBA / AllofUs",
+     client: "NBA | AllofUs",
      gif: "nbabasketball.mp4"
   }, 
   {
@@ -116,13 +116,13 @@ const gifs = [
   {
     day: "011",
     content: "Using facial recognition to detect when the HMI needs to display for the driver.",
-    client: "Toyota / ustwo",
+    client: "Toyota | ustwo",
     gif: "toyotafocus.mp4"
   },  
   {
     day: "010",
     content: "When balance and vision don't match, car sickness occurs. Alexa I Spy solves that using real-time objects to spy.",
-    client: "Ford / HMSD",
+    client: "Ford | HMSD",
     gif: "fordechoauto.mp4"
   },  
   {
@@ -140,7 +140,7 @@ const gifs = [
   {
     day: "007",
     content: "Start your car, from your watch - just like James Bond.",
-    client: "JLR / ustwo",
+    client: "JLR | ustwo",
     gif: "jlrremote.mp4"
   },  
   {
@@ -152,7 +152,7 @@ const gifs = [
   {
     day: "005",
     content: "Context is key(board). An on-screen keyboard with centred spacebar can save input time.",
-    client: "Samsung / AllofUs",
+    client: "Samsung | AllofUs",
     gif: "keyboard.mp4"
   },
     {
@@ -164,19 +164,19 @@ const gifs = [
   {
     day: "03",
     content: "Dynamic navigation that meets you where most comfortable.",
-    client: "CenterPlan / Unit9",
+    client: "CenterPlan | Unit9",
     gif: "centerplan.mp4"
   },
   {
     day: "002",
     content: "Indicate driver's speed exceeds the road's limit with peripheral colour.",
-    client: "Citroën / ustwo",
+    client: "Citroën | ustwo",
     gif: "cluster.mp4"
   },
   {
     day: "001",
     content: "We used haptic screens that use sound waves to mimic the feel of buttons for the blind.",
-    client: "VW / ustwo",
+    client: "VW | ustwo",
     gif: "vwblindscreen.mp4"
   }
 ];
