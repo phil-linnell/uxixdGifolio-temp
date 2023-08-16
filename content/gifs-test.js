1d78a38611bfe774b75142d78915403486102b1c
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-08-16 04:10:40 +0100

    Update gifs-test.js

--- content/gifs-test.js
+++ content/gifs-test.js
@@ -8,13 +8,13 @@ const gifs = [
    },
    {
     day: "070",
-    content: "Gamification and play theory sit behind this touchscreen interactive that helps young visitors learn.",
+    content: "Gamification and play theory sit behind this touchscreen game that helps young visitors learn.",
     client: "Science Museum",
     gif: "smgoodbye.mp4"
    },
    {
     day: "069",
-    content: "In our book, we speculated that sex might be something to do in driverless cars, coining the phrase 'The Last Mile Club'.",
+    content: "In our book, we speculated that sex might be something to do in driverless cars, coining the term 'The Last Mile Club'.",
     client: "ustwo",
     gif: "lastmileclub.mp4"
    },
