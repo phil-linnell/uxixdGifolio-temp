d7f0e446bc3567085f30da4a61d38e75dcda0e42
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-09-11 18:40:57 +0100

    Update gifs-games.js

--- content/gifs-games.js
+++ content/gifs-games.js
@@ -1,16 +1,77 @@
 const gifsGames = [
+
+  {
+    day: "012",
+    content: "Innovative packaging with a double pull-to-open mechanic, exposing secret compartments.",
+    client: "PlayStation",
+    gif: "psclank.mp4"
+  },
+  {
+    day: "011",
+    content: "An ever-telescoping Mount Olympus special edition for this Greek God themed PlayStation franchise.",
+    client: "PlayStation",
+    gif: "gow.mp4"
+  },
+  {
+    day: "010",
+    content: "Gamification and play theory sit behind this touchscreen game that helps young visitors learn.",
+    client: "Science Museum",
+    gif: "smgoodbye.mp4"
+  },
+  {
+    day: "009",
+    content: "An online phygital Top Trumps game with physical cards and digital AR components.",
+    client: "PlayStation",
+    gif: "playstationar.mp4"
+  },
+  {
+    day: "008",
+    content: "Fun for a Saturday. An educational Flash game I designed and co-developed.",
+    client: "British Museum",
+    gif: "britishmuseum.mp4"
+  }, 
+  {
+    day: "007",
+    content: "Simple interactions are often best, like this gaming radial dial + button combo.",
+    client: "Science Museum",
+    gif: "greenhouse.mp4"
+  },
+  {
+    day: "006",
+    content: "IXD can bring brand delight to packaging - or a raspberry from a virtual pet.",
+    client: "PlayStation",
+    gif: "eyepet.mp4"
+  },
+  {
+    day: "005",
+    content: "Keep your eye on the cup... Did you see the horse? Based on 'Selective Awareness' by Simons & Chabris.",
+    client: "Science Museum",
+    gif: "smcuptrick.mp4"
+  },
+  {
+    day: "004",
+    content: "Real sand is driven into reclaimed tyres for this off-road racing game.",
+    client: "PlayStation",
+    gif: "sandkit.mp4"
+  },
+  {
+    day: "003",
+    content: "Full branding, UI and HUD design for PlayStation's PlayTV device.",
+    client: "PlayStation",
+    gif: "psplaytv.mp4"
+  },
   {
     day: "002",
-    content: "Indicate driver's speed exceeds the road's limit with peripheral colour.",
-    client: "Citroën | ustwo",
-    gif: "cluster.mp4"
+    content: "Design your island on the physical game-board as it comes to life with AR on screen.",
+    client: "Science Museum",
+    gif: "sciencemuseum.mp4"
   },
   {
     day: "001",
-    content: "Where it all began. Build fast, learn fast. The prototypical approach to interaction design.",
-    client: "Toyota | ustwo",
-    gif: "toyotabuck.mp4"
-  }
+    content: "An unboxing narrative that releases components in order of setup steps.",
+    client: "PlayStation",
+    gif: "playtvunbox.mp4"
+  },
 ];
 
 export default gifsGames;
