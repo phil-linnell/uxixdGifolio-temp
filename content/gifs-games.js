42f714bd0487d0c12908ad633cb486cdd4b294ca
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-09-14 13:20:40 +0100

    Update gifs-games.js

--- content/gifs-games.js
+++ content/gifs-games.js
@@ -1,76 +1,70 @@
 const gifsGames = [
 
   {
-    day: "012",
-    content: "Innovative packaging with a double pull-to-open mechanic, exposing secret compartments.",
-    client: "PlayStation",
-    gif: "psclank.mp4"
-  },
-  {
     day: "011",
-    content: "An ever-telescoping Mount Olympus special edition for this Greek God themed PlayStation franchise.",
-    client: "PlayStation",
-    gif: "gow.mp4"
-  },
-  {
-    day: "010",
-    content: "Gamification and play theory sit behind this touchscreen game that helps young visitors learn.",
-    client: "Science Museum",
-    gif: "smgoodbye.mp4"
-  },
-  {
-    day: "009",
     content: "An online phygital Top Trumps game with physical cards and digital AR components.",
     client: "PlayStation",
     gif: "playstationar.mp4"
   },
   {
-    day: "008",
-    content: "Fun for a Saturday. An educational Flash game I designed and co-developed.",
-    client: "British Museum",
-    gif: "britishmuseum.mp4"
-  }, 
-  {
-    day: "007",
-    content: "Simple interactions are often best, like this gaming radial dial + button combo.",
-    client: "Science Museum",
-    gif: "greenhouse.mp4"
+    day: "010",
+    content: "Innovative packaging with a double pull-to-open mechanic, exposing secret compartments.",
+    client: "PlayStation",
+    gif: "psclank.mp4"
   },
   {
-    day: "006",
+    day: "009",
+    content: "An ever-telescoping Mount Olympus special edition for this Greek God themed PlayStation franchise.",
+    client: "PlayStation",
+    gif: "gow.mp4"
+  },
+  {
+    day: "008",
     content: "IXD can bring brand delight to packaging - or a raspberry from a virtual pet.",
     client: "PlayStation",
     gif: "eyepet.mp4"
   },
   {
-    day: "005",
-    content: "Keep your eye on the cup... Did you see the horse? Based on 'Selective Awareness' by Simons & Chabris.",
-    client: "Science Museum",
-    gif: "smcuptrick.mp4"
-  },
-  {
-    day: "004",
+    day: "007",
     content: "Real sand is driven into reclaimed tyres for this off-road racing game.",
     client: "PlayStation",
     gif: "sandkit.mp4"
   },
   {
-    day: "003",
+    day: "006",
     content: "Full branding, UI and HUD design for PlayStation's PlayTV device.",
     client: "PlayStation",
     gif: "psplaytv.mp4"
   },
   {
-    day: "002",
-    content: "Design your island on the physical game-board as it comes to life with AR on screen.",
-    client: "Science Museum",
-    gif: "sciencemuseum.mp4"
-  },
-  {
-    day: "001",
+    day: "005",
     content: "An unboxing narrative that releases components in order of setup steps.",
     client: "PlayStation",
     gif: "playtvunbox.mp4"
+  },
+    {
+    day: "004",
+    content: "Fun for a Saturday. An educational Flash game I designed and co-developed.",
+    client: "British Museum",
+    gif: "britishmuseum.mp4"
+  }, 
+  {
+    day: "003",
+    content: "Simple interactions are often best, like this gaming radial dial + button combo.",
+    client: "Science Museum",
+    gif: "greenhouse.mp4"
+  },
+    {
+    day: "002",
+    content: "Keep your eye on the cup... Did you see the horse? Based on 'Selective Awareness' by Simons & Chabris.",
+    client: "Science Museum",
+    gif: "smcuptrick.mp4"
+  },
+    {
+    day: "001",
+    content: "Design your island on the physical game-board as it comes to life with AR on screen.",
+    client: "Science Museum",
+    gif: "sciencemuseum.mp4"
   },
 ];
 
