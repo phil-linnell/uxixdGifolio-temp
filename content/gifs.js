87b3febc37f5037856f6a798ebe25af9cc62b180
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-09-21 11:00:59 +0100

    Update gifs.js

--- content/gifs.js
+++ content/gifs.js
@@ -1,6 +1,12 @@
 const gifs = [
 
   {
+    day: "053",
+    content: "Physical controls build muscle memory that digital controls don't do as well. We explore which to use when.",
+    client: "ustwo",
+    gif: "musclememory.mp4"
+  },
+  {
     day: "052",
     content: "Testing a sound + haptic prototype that guides blind users to their designated driverless taxi.",
     client: "VW | ustwo",
