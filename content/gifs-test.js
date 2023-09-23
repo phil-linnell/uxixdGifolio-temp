3e08b13d20ac9c46dce9aec72b9db52e4875e52b
UXIXD <140653004+UXIXD-ltd@users.noreply.github.com>
2023-09-23 01:43:49 +0100

    Update gifs-test.js

--- content/gifs-test.js
+++ content/gifs-test.js
@@ -7,7 +7,7 @@ const gifsTest = [
     gif: "dicetemp.mp4"
   },
    {
-    day: "095",
+    day: "094",
     content: "We disected a chat between Michael Knight and K.I.T.T. to start understanding how we can talk to driverless cars.",
     client: "ustwo",
     gif: "avchat.mp4"
